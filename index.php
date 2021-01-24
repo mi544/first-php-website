12dca4230f0bcd39326f9eac5e30eae6fa150e79
Maksim Verkhoturov <sd32@protonmail.com>
2021-01-24 10:17:09 -0700

    adds first php file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+
+<body>
+    Hello!
+    <?php
+        echo $_SERVER['HTTP_USER_AGENT'];
+        $abc = '123';
+        echo $abc;
+        phpinfo();
+    ?>
+</body>
+
+</html>
